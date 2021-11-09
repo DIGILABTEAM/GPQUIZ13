6bffcd2ff558ee594afe63387d230fec23d23bb2
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-11-09 10:55:53 +0530

    deployment
    project deployment css js html

---
+++ js/questions.js
@@ -0,0 +1,45 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "In history of patient with cough, what drug in particular do we ask about",
+				option1: "Beta blockers",
+				option2: "ACE inhibitors",
+				option3: "ARB",
+				option4: "Diuretics",
+				answer: 2
+			},
+			{
+				question: "What does breathlessness on exertion point to?",
+				option1: "Asthma",
+				option2: "COPD",
+				option3: "Pneumonia",
+				option4: "All of the above",
+				answer: 4
+			},
+			{
+				question: "when should be Systemic antitussives recommended",
+				option1: "For a productive cough that interferes with sleep",
+				option2: "Any type of cough",
+				option3: "Usually not recommended",
+				answer: 1
+			},
+			{
+				question: "The following antitussive is present in opium but has no analgesic or addicting properties:",
+				option1: "Noscapine",
+				option2: "Codeine",
+				option3: "Pholcodeine",
+				option4: "Ethylmorphine",
+				answer: 1
+			},
+			{
+				question: "Mucokinetic is a drug which:",
+				option1: "PneumoniaReduces airway mucus secretion",
+				option2: "Increases airway mucus secretion",
+				option3: "Makes respiratory secretions more watery",
+				option4: "Stimulates mucociliary activity of bronchial epithelium",
+				answer: 3
+			}
+		]
+	];
